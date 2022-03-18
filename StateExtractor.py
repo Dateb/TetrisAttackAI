d8059db4120ed4f449381adfc816d33d9ca7be5b
Daniel Tebart <daniel.tebart@rwth-aachen.de>
2022-03-18 01:59:18 +0100

    Implemented block state extraction by reading key pixels of interest.

---
+++ StateExtractor.py
@@ -0,0 +1,45 @@
+from typing import List
+
+import numpy as np
+from numpy import ndarray
+
+from BlockType import BlockType
+
+
+class StateExtractor:
+
+    def __init__(self):
+        self.__block_pixel_radius = 8
+        self.__n_block_rows = 12
+        self.__n_block_cols = 6
+
+    def extract_board_state_key_points(self, image) -> ndarray:
+        key_points = np.zeros((self.__n_block_rows, self.__n_block_cols))
+
+        for row in range(self.__n_block_rows):
+            for col in range(self.__n_block_cols):
+                key_points[row, col] = self.__get_block_value_of_key_point(image, row, col)
+
+        return key_points
+
+    def __get_block_value_of_key_point(self, image, row: int, col: int):
+        x_coord = 2 * col * self.__block_pixel_radius + self.__block_pixel_radius
+        y_coord = 2 * row * self.__block_pixel_radius + self.__block_pixel_radius
+
+        return self.__get_block_type_of_rgb_values(image[y_coord, x_coord]).value
+
+    def __get_block_type_of_rgb_values(self, rgb_values: List[int]) -> BlockType:
+        red_value, green_value, blue_value = rgb_values
+        color_threshold = 240
+        if red_value > color_threshold and green_value > color_threshold:
+            return BlockType.STAR
+        if red_value > color_threshold and blue_value > color_threshold:
+            return BlockType.DIAMOND
+        if green_value > color_threshold and blue_value > color_threshold:
+            return BlockType.TRIANGLE
+        if red_value > color_threshold:
+            return BlockType.HEART
+        if green_value > color_threshold:
+            return BlockType.CIRCLE
+        return BlockType.BACKGROUND
+
--- TetrisAttackProcessor.py
+++ TetrisAttackProcessor.py
@@ -6,18 +6,25 @@ from rl.core import Processor
 
 #if self.processor is not None:
 #    action = self.processor.process_action(action)
+from StateExtractor import StateExtractor
+
 
 class TetrisAttackProcessor(Processor):
 
     def __init__(self, n_actions: int):
         super(TetrisAttackProcessor, self).__init__()
         self.__n_actions = n_actions
+        self.__state_extractor = StateExtractor()
 
     def process_observation(self, observation):
         img_arr = observation
-        img_arr = img_arr[20:215, 86:286]
+        img_arr = img_arr[22:215, 88:185]
+
+        key_points = self.__state_extractor.extract_board_state_key_points(img_arr)
+        print(key_points)
+
         im = Image.fromarray(img_arr)
-        im = im.resize((80, 80))
+        #im = im.resize((80, 80))
         im.save("example.jpeg")
         return np.asarray(PIL.Image.open("example.jpeg"))
 
